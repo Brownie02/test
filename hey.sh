21ffd1456525b957510cc07c3b796e3de796a99c
Brownie02 <87292113+Brownie02@users.noreply.github.com>
2021-10-19 00:07:23 +0100

    Update hey.sh

--- hey.sh
+++ hey.sh
@@ -1,7 +1,7 @@
 echo "What is your name?"
 read name 
-if [$name]; then 
-echo "Hi $name, Brown Dhanyiel sends his greetings"
+if [ $name ]; then 
+echo "Hi $name Brown Dhanyiel sends his greetings"
 else 
-echo " Come man, are you kidding me? "
+echo " Comeon man, are you kidding me? "
 fi
